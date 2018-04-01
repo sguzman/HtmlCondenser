2cf10958751bb16422469d0d028629bfd2f46305
Salvador Guzman <guzmansalv@gmail.com>
2018-03-31 17:40:59 -0700

    add Condenser object

---
+++ condenser/src/com/github/sguzman/htmlcondenser/Condenser.scala
@@ -0,0 +1,23 @@
+package com.github.sguzman.htmlcondenser
+
+/**
+  * <h1>HtmlCondenser Class</h1>
+  * <p>
+  * This represents the main class of the entire project. The
+  * user of will access any and all the functionality contained
+  * within this project via any public methods accessible here.
+  * </p>
+  * <p>
+  * This class should have mainly a single method that the user
+  * will be interested in. This will be the call to the condenser's
+  * subsystems. From this single interface alone, the user should
+  * be able to parameterize what kind of customizable actions
+  * should be taken while parsing (e.g. omit <script></script> tags,
+  * omit <style></style> tags, omit comments)
+  * </p>
+  *
+  * @author Salvador Guzman (guzmansalv@gmail.com)
+  */
+object Condenser {
+
+}
