c796293172a6f3f0a0c808270741cbc66e34d559
Salvador Guzman <guzmansalv@gmail.com>
2018-03-31 20:16:46 -0700

    test case for comments

--- condenser/test/src/com/github/sguzman/htmlcondenser/CondenserTest.scala
+++ condenser/test/src/com/github/sguzman/htmlcondenser/CondenserTest.scala
@@ -2,8 +2,18 @@ package com.github.sguzman.htmlcondenser
 
 import org.scalatest.FlatSpec
 
+import scala.io.Source
+
 class CondenserTest extends FlatSpec {
   "An HTML condenser" should "remove comments" in {
-    assert(true)
+    val inputPath = "./condenser/test/resources/comments/comments-input.html"
+    val outputPath = "./condenser/test/resources/comments/comments-output.html"
+
+    val input = Source.fromFile(inputPath).getLines.mkString("\n")
+    val actual = Condenser.condenseString(input)
+
+    val output = Source.fromFile(outputPath).getLines.mkString("\n")
+
+    assert(actual == output)
   }
 }
