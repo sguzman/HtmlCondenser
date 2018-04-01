09200808b2e6455df3c07a1a10301ed8094e7bc6
Salvador Guzman <guzmansalv@gmail.com>
2018-04-01 00:04:05 -0700

    idempotent test cases

--- condenser/test/src/com/github/sguzman/htmlcondenser/CondenserTest.scala
+++ condenser/test/src/com/github/sguzman/htmlcondenser/CondenserTest.scala
@@ -17,6 +17,20 @@ class CondenserTest extends FlatSpec {
     assert(actual == output)
   }
 
+  def idempotentTest(name: String): Unit = {
+    val inputPath = s"./condenser/test/resources/$name/$name-input.html"
+    val input = Source.fromFile(inputPath).getLines.mkString("\n")
+
+    val actual = Condenser.condenseString(input)
+    val actual2 = Condenser.condenseString(actual)
+    val actual3 = Condenser.condenseString(actual2)
+    val actual4 = Condenser.condenseString(actual3)
+
+    assert(actual == actual2)
+    assert(actual2 == actual3)
+    assert(actual3 == actual4)
+  }
+
   "An HTML condenser" should "remove comments" in {
     testExample("comments")
   }
@@ -35,5 +49,13 @@ class CondenserTest extends FlatSpec {
 
   "An HTML condenser" should "condense a behemoth HTML doc" in {
     testExample("inline-behemoth")
+  }
+
+  "An HTML condenser" should "be idempotent" in {
+    idempotentTest("comments")
+    idempotentTest("simple")
+    idempotentTest("github-html5-template")
+    idempotentTest("facebook-03-31-18")
+    idempotentTest("inline-behemoth")
   }
 }
